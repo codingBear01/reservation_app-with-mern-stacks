c39eea5a336a65da0be01755b1f2288c7b03385d
mameMan <jokingbear01@gmail.com>
2022-05-09 23:10:43 +0900

    fin client

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -1,38 +1,93 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import NavBar from './../../components/nav-bar/NavBar';
 import Header from './../../components/header/Header';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCircleArrowLeft,
+  faCircleArrowRight,
+  faCircleXmark,
+  faLocationDot,
+} from '@fortawesome/free-solid-svg-icons';
 import MailLists from './../../components/mailLists/MailLists';
 import Footer from './../../components/footer/Footer';
 
 const Hotel = () => {
+  const [sliderNum, setSliderNum] = useState(0);
+  const [openSlider, setOpenSlider] = useState(false);
   const imgs = [
     {
-      src: 'https://pix10.agoda.net/hotelImages/2962286/-1/16d4e517a27fb6c76de2cd9e65e9c3ed.jpg?ca=6&ce=1&s=1024x768',
+      src: 'https://i0.wp.com/blog.allstay.com/wp-content/uploads/2021/02/7-10.jpg?resize=740%2C505&ssl=1',
+    },
+    {
+      src: 'https://cf.bstatic.com/xdata/images/hotel/max500/117339302.jpg?k=cafe09480cfc29272d206599c864892970ee7ef8b1b737505baafcb7e18511f2&o=&hp=1',
+    },
+    {
+      src: 'https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/1jPF/image/s5peAAk69VagVdOLjikjeD2ial0.png',
     },
     {
       src: 'https://pix10.agoda.net/hotelImages/2962286/-1/16d4e517a27fb6c76de2cd9e65e9c3ed.jpg?ca=6&ce=1&s=1024x768',
     },
     {
-      src: 'https://pix10.agoda.net/hotelImages/2962286/-1/16d4e517a27fb6c76de2cd9e65e9c3ed.jpg?ca=6&ce=1&s=1024x768',
+      src: 'https://pix10.agoda.net/hotelImages/296/2962286/2962286_17110308130058663940.jpg?ca=13&ce=1&s=1024x768',
     },
     {
-      src: 'https://pix10.agoda.net/hotelImages/2962286/-1/16d4e517a27fb6c76de2cd9e65e9c3ed.jpg?ca=6&ce=1&s=1024x768',
-    },
-    {
-      src: 'https://pix10.agoda.net/hotelImages/2962286/-1/16d4e517a27fb6c76de2cd9e65e9c3ed.jpg?ca=6&ce=1&s=1024x768',
-    },
-    {
-      src: 'https://pix10.agoda.net/hotelImages/2962286/-1/16d4e517a27fb6c76de2cd9e65e9c3ed.jpg?ca=6&ce=1&s=1024x768',
+      src: 'https://image.goodchoice.kr/resize_490x348/affiliate/2020/11/12/5facd1c4769bd.jpg',
     },
   ];
+
+  const handleOpenSlider = useCallback((i) => {
+    setSliderNum(i);
+    setOpenSlider(true);
+  }, []);
+
+  const handleChangeImg = useCallback(
+    (direction) => {
+      let newSliderNum;
+
+      if (direction === 'l') {
+        newSliderNum = sliderNum === 0 ? imgs.length - 1 : sliderNum - 1;
+      } else {
+        newSliderNum = sliderNum === imgs.length - 1 ? 0 : sliderNum + 1;
+      }
+
+      console.log(newSliderNum);
+
+      setSliderNum(newSliderNum);
+    },
+    [imgs.length, sliderNum]
+  );
+
   return (
     <>
       <NavBar />
       <Header type="list" />
       <div className="hotelContainer">
-        <div className="slider"></div>
+        {openSlider && (
+          <div className="slider">
+            <FontAwesomeIcon
+              className="closeIcon"
+              icon={faCircleXmark}
+              onClick={() => setOpenSlider(false)}
+            />
+            <FontAwesomeIcon
+              className="arrow"
+              icon={faCircleArrowLeft}
+              onClick={() => handleChangeImg('l')}
+            />
+            <div className="sliderWrapper">
+              <img
+                src={imgs[sliderNum].src}
+                alt="slider-img"
+                className="sliderImg"
+              />
+            </div>
+            <FontAwesomeIcon
+              className="arrow"
+              icon={faCircleArrowRight}
+              onClick={() => handleChangeImg('r')}
+            />
+          </div>
+        )}
         <div className="hotelWrapper">
           <button className="bookNow">Reserve or Book Now!</button>
           <h1 className="hotelTitle">Grand Hotel</h1>
@@ -49,7 +104,12 @@ const Hotel = () => {
           <div className="hotelImgs">
             {imgs.map((img, i) => (
               <div key={i} className="hotelImgWrapper">
-                <img src={img.src} alt="hotel" className="hotelImg" />
+                <img
+                  onClick={() => handleOpenSlider(i)}
+                  src={img.src}
+                  alt="hotel"
+                  className="hotelImg"
+                />
               </div>
             ))}
           </div>
